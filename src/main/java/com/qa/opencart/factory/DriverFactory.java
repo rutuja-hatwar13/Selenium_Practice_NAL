f59390fa77921eb82ce60663ad119bd52d46fc93
rutujahatwar <hatwar.rutuja91@gmail.com>
2025-09-05 17:38:22 +0530

    Updated commons page file

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -6,6 +6,9 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
+import com.qa.opencart.exceptions.FrameworkExceptions;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
@@ -14,9 +17,7 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
-import com.aventstack.chaintest.plugins.ChainTestListener;
 import com.qa.opencart.errors.AppError;
-import com.qa.opencart.exceptions.FrameworkExceptions;
 
 public class DriverFactory {
 
@@ -27,39 +28,44 @@ public class DriverFactory {
 
     public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<WebDriver>();
 
+    private static final Logger log = LogManager.getLogger(DriverFactory.class);
+
+    public OptionsManager optionsManager;
+
     /**
      * This method is init the driver on the basis of browser...
+     *
      * @param browserName
      * @return it returns driver
      */
     public WebDriver initDriver(Properties prop) {
 
         String browserName = prop.getProperty("browser");
-        System.out.println("browser name : " + browserName);
+        // System.out.println("browser name : " + browserName);
+        log.info("browser name : " + browserName);
 
         highlightEle = prop.getProperty("highlight");
+        optionsManager = new OptionsManager(prop);
 
         switch (browserName.trim().toLowerCase()) {
             case "chrome":
-                // driver = new ChromeDriver();
-                tlDriver.set(new ChromeDriver());
+                tlDriver.set(new ChromeDriver(optionsManager.getChromeOptions()));
                 break;
             case "firefox":
-                // driver = new FirefoxDriver();
-                tlDriver.set(new FirefoxDriver());
+                tlDriver.set(new FirefoxDriver(optionsManager.getFirefoxOptions()));
                 break;
             case "edge":
-                // driver = new EdgeDriver();
-                tlDriver.set(new EdgeDriver());
+                tlDriver.set(new EdgeDriver(optionsManager.getEdgeOptions()));
                 break;
             case "safari":
-                // driver = new SafariDriver();
                 tlDriver.set(new SafariDriver());
                 break;
-
             default:
-                System.out.println(AppError.INVALID_BROWSER_MESG + " : " + browserName);
+                log.error(AppError.INVALID_BROWSER_MESG + " : " + browserName);
+                FrameworkExceptions fe = new FrameworkExceptions(AppError.INVALID_BROWSER_MESG + " : " + browserName);
+                log.error("Exception occurred while initializing driver: ", fe);
                 throw new FrameworkExceptions("=====INVALID BROWSER====");
+
         }
 
         getDriver().manage().deleteAllCookies();
@@ -81,18 +87,59 @@ public class DriverFactory {
 
     /**
      * This method is init the prop with properties file...
+     *
      * @return
      */
+
+    // mvn clean install -Denv="qa"
+    // mvn clean install
+    // mvn clean install -Dsurefire.suiteXmlFiles=src/test/resources/testrunners/testng_regression.xml -Denv="dev"
     public Properties initProp() {
         prop = new Properties();
+        FileInputStream ip = null;
+
+        String envName = System.getProperty("env");
+        log.info("Env name =======>" + envName);
+
         try {
-            FileInputStream ip = new FileInputStream("./src/test/resources/config/config.properties");
-            prop.load(ip);
+            if (envName == null) {
+                log.warn("no env.. is passed, hence running tcs on QA environment...by default..");
+                ip = new FileInputStream("./src/test/resources/config/config.qa.properties");
+            }
+
+            else {
+                switch (envName.trim().toLowerCase()) {
+                    case "qa":
+                        ip = new FileInputStream("./src/test/resources/config/config.qa.properties");
+                        break;
+                    case "stage":
+                        ip = new FileInputStream("./src/test/resources/config/config.stage.properties");
+                        break;
+                    case "uat":
+                        ip = new FileInputStream("./src/test/resources/config/config.uat.properties");
+                        break;
+                    case "dev":
+                        ip = new FileInputStream("./src/test/resources/config/config.dev.properties");
+                        break;
+                    case "prod":
+                        ip = new FileInputStream("./src/test/resources/config/config.properties");
+                        break;
+                    default:
+                        log.error("Env value is invalid...plz pass the right env value..");
+                        throw new FrameworkExceptions("====INVALID ENVIRONMENT====");
+                }
+            }
+
         } catch (FileNotFoundException e) {
             e.printStackTrace();
+        }
+
+        try {
+            prop.load(ip);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         return prop;
     }
 
@@ -114,4 +161,5 @@ public class DriverFactory {
         return ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BASE64);// temp dir
 
     }
+
 }
--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -4,9 +4,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import com.qa.opencart.constants.AppConstants;
-import com.qa.opencart.factory.DriverFactory;
 import com.qa.opencart.utils.ElementUtil;
 
 import io.qameta.allure.Step;
@@ -16,39 +16,40 @@ public class LoginPage {
     private WebDriver driver;
     private ElementUtil eleUtil;
 
-    //private By locators: page objects
+    // private By locators: page objects
     private final By emailID = By.id("input-email");
     private final By password = By.id("input-password");
     private final By loginBtn = By.xpath("//input[@value='Login']");
-    private final By forgotPwdLink = By.linkText("Forgotten Password11");
+    private final By forgotPwdLink = By.linkText("Forgotten Password");
     private final By header = By.tagName("h2");
     private final By registerLink = By.linkText("Register");
 
+    private final By loginErrorMessg = By.cssSelector("div.alert.alert-danger.alert-dismissible");
+
     private static final Logger log = LogManager.getLogger(LoginPage.class);
 
-
-    //public constructor
+    // public constructor
     public LoginPage(WebDriver driver) {
         this.driver = driver;
         eleUtil = new ElementUtil(driver);
 
     }
 
-    //public page methods/actions
+    // public page methods/actions
 
     @Step("getting login page title....")
     public String getLoginPageTitle() {
         String title = eleUtil.waitForTitleIs(AppConstants.LOGIN_PAGE_TITLE, AppConstants.DEFAULT_SHORT_WAIT);
-        //System.out.println("login page title: "+ title);
-        log.info("login page title: "+ title);
+        // System.out.println("login page title: "+ title);
+        log.info("login page title: " + title);
         return title;
     }
 
     @Step("getting login url title....")
     public String getLoginPageURL() {
         String url = eleUtil.waitForURLContains(AppConstants.LOGIN_PAGE_FRACTION_URL, AppConstants.DEFAULT_SHORT_WAIT);
-        //System.out.println("login page url: "+ url);
-        log.info("login page url : "+ url);
+        // System.out.println("login page url: "+ url);
+        log.info("login page url : " + url);
         return url;
     }
 
@@ -62,14 +63,32 @@ public class LoginPage {
         return eleUtil.isElementDisplayed(header);
     }
 
-    @Step("login with username: {0} and password: {1}")
+    @Step("login with correct username: {0} and password: {1}")
     public AccountsPage doLogin(String appUsername, String appPassword) {
-        //System.out.println("Application credentials: "+ appUsername + ":"+ appPassword);
-        log.info("application credentials: "+ appUsername + " : " + appPassword);
+        log.info("application credentials: " + appUsername + " : " + "*********");
         eleUtil.waitForElementVisible(emailID, AppConstants.DEFAULT_MEDIUM_WAIT).sendKeys(appUsername);
         eleUtil.doSendKeys(password, appPassword);
         eleUtil.doClick(loginBtn);
         return new AccountsPage(driver);
+    }
+
+    @Step("login with in-correct username: {0} and password: {1}")
+    public boolean doLoginWithInvalidCredentails(String invalidUN, String invalidPWD) {
+        log.info("Invalid application credentials: " + invalidUN + " : " + invalidPWD);
+        WebElement emailEle = eleUtil.waitForElementVisible(emailID, AppConstants.DEFAULT_MEDIUM_WAIT);
+        emailEle.clear();
+        emailEle.sendKeys(invalidUN);
+        eleUtil.doSendKeys(password, invalidPWD);
+        eleUtil.doClick(loginBtn);
+        String errorMessg = eleUtil.doElementGetText(loginErrorMessg);
+        log.info("invalid creds error messg: " + errorMessg);
+        if (errorMessg.contains(AppConstants.LOGIN_BLANK_CREDS_MESSG)) {
+            return true;
+        }
+        else if (errorMessg.contains(AppConstants.LOGIN_INVALID_CREDS_MESSG)) {
+            return true;
+        }
+        return false;
     }
 
     @Step("navigating to register page...")
